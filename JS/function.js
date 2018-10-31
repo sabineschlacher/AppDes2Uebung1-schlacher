831dede796f26bd94e897ad45797ad886e37e76d
Sabine Schlacher <hi@sabineschlacher.com>
2018-10-31 12:45:40 +0100

    asdfas
    adfasd

--- JS/function.js
+++ JS/function.js
@@ -1,10 +1,23 @@
-/**
+	/**
  * @author Matthias Reischer
  * Sample File for Javascript functionality
  *
  */
 
-
+/*
 function printSomeThing() {
     alert("Button pressed!");
-}
+}*/
+
+    function bildTausch(){
+
+    	//console.log(document.getElementById("bild1").src);
+
+    if (document.getElementById("bild").src.includes ("images/yeah.gif")){
+
+        document.getElementById("bild").src = "images/congratulation.gif";
+
+        } else {
+        document.getElementById("bild").src = "images/yeah.gif";
+    }
+    }
